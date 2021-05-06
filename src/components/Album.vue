e820dda0fe6c5354cb2eba24225337cf36bad137
GioOli7 <gio07live@gmail.com>
2021-05-06 15:39:34 +0200

    props

---
+++ src/components/Album.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>{{ details.title }}</div>
+</template>
+
+<script>
+	export default {
+		name: 'Album',
+		props: ['details'],
+	};
+</script>
+
+<style></style>
--- src/components/AlbumList.vue
+++ src/components/AlbumList.vue
@@ -1,12 +1,42 @@
 <template>
 	<div>
-		<h1>adsfasd</h1>
+		<Album v-for="(album, index) in albums" :key="index" :details="album" />
 	</div>
 </template>
 
 <script>
+	import axios from 'axios';
+	import Album from '@/components/Album';
+
 	export default {
 		name: 'AlbumList',
+		components: {
+			Album,
+		},
+		data() {
+			return {
+				// my data
+				albums: [],
+			};
+		},
+		created() {
+			this.getAlbums();
+		},
+		methods: {
+			// my methods
+			getAlbums() {
+				axios
+					.get(
+						'https://flynn.boolean.careers/exercises/api/array/music'
+					)
+					.then((result) => {
+						this.albums = result.data.response;
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+			},
+		},
 	};
 </script>
 
